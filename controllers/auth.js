cc2cc3692b45bbb0b13362ee54ccdf970ae672ad
Rakshit6722 <rakshitchauhan435@gmail.com>
2024-11-20 19:03:36 +0530

    package.json

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,11 @@
 const User = require('../models/User')
 const Posts = require('../models/Posts')
 
+exports.signUp = async (req, res) => {
+    try{
+        const {userName, email, password} = req.body
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({message:"Internal Server Error"})
+    }
+}
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const dbConnect = require('./config/datatbase')
 const cloudinaryConnect = require('./config/cloudinary')
 require('dotenv').config()
+const fileUpload = require('express-fileupload')
 
 const app = express()
 
 app.use(express.json)
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:'/tmp'
+}))
 
 const PORT = process.env.PORT || 8000
 
